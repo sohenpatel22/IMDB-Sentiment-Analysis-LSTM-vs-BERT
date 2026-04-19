185af5c307c03daa4453a9ade04fca060281dee3
SohenPatel <sohen.patel@mail.utoronto.ca>
2026-04-19 08:06:45 -0400

    Add BERT tokenizer utility

--- src/data/tokenizer_utils.py
+++ src/data/tokenizer_utils.py
@@ -4,5 +4,4 @@ MODEL_NAME = "bert-base-cased"
 
 
 def get_tokenizer():
-    tokenizer = BertTokenizer.from_pretrained(MODEL_NAME)
-    return tokenizer
+    return BertTokenizer.from_pretrained(MODEL_NAME)
